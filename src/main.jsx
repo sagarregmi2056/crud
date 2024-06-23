9e3d8e76caafcf86fa8d8bedc74842ad75002d13
sagar regmi <115308062+sagarregmi2056@users.noreply.github.com>
2024-06-23 12:26:33 +0545

    added url yt

--- src/main.jsx
+++ src/main.jsx
@@ -10,4 +10,8 @@ ReactDOM.createRoot(document.getElementById("root")).render(
   </BrowserRouter>
 );
 
-// git hub links https://github.com/bezkoder/react-crud-web-api
+// git hub links
+
+https://github.com/bezkoder/react-crud-web-api
+https://www.youtube.com/watch?v=2k4dfsPjGoY&list=PL5X0rgxaTIuOWHedpnXq_jqFD5DmkFQWD&index=7
+https://www.youtube.com/watch?v=maGN8iGs744&t=305s
